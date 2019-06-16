74831c594bfc8f94e0b77a23753ef34d2f71d0bb
Mikołaj Płachta <mixxenxd@wp.pl>
2019-06-16 22:35:35 +0200

    Initial Commit

---
+++ MusicPlaceSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlaceSearcher
+//
+//  Created by Mikołaj-iMac on 16/06/2019.
+//  Copyright © 2019 Mikołaj Płachta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
